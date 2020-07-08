8b93df30da73671c40ef0754fe01ba0b3bd4e588
charles <celliwig@nym.hush.com>
2020-07-08 01:31:47 +0000

    Removed extraneous code from board file.

--- board/meru/ap320/ap320.c
+++ board/meru/ap320/ap320.c
@@ -178,63 +178,6 @@ int misc_init_f(void)
 	unsigned int *gpio_reg = 0xE0000c08;
 	*gpio_reg = 0x3d800000;			// Status LED to green
 
-//#ifdef CONFIG_VSC7385_ENET
-//	volatile u32 *vsc7385_cpuctrl;
-//
-//	/* 0x1c0c0 is the VSC7385 CPU Control (CPUCTRL) Register.  The power up
-//	   default of VSC7385 L1_IRQ and L2_IRQ requests are active high.  That
-//	   means it is 0 when the IRQ is not active.  This makes the wire-AND
-//	   logic always assert IRQ7 to CPU even if there is no request from the
-//	   switch.  Since the compact flash and the switch share the same IRQ,
-//	   the Linux kernel will think that the compact flash is requesting irq
-//	   and get stuck when it tries to clear the IRQ.  Thus we need to set
-//	   the L2_IRQ0 and L2_IRQ1 to active low.
-//
-//	   The following code sets the L1_IRQ and L2_IRQ polarity to active low.
-//	   Without this code, compact flash will not work in Linux because
-//	   unlike U-Boot, Linux uses the IRQ, so this code is necessary if we
-//	   don't enable compact flash for U-Boot.
-//	 */
-//
-//	vsc7385_cpuctrl = (volatile u32 *)(CONFIG_SYS_VSC7385_BASE + 0x1c0c0);
-//	*vsc7385_cpuctrl |= 0x0c;
-//#endif
-
-//#ifdef CONFIG_COMPACT_FLASH
-//	/* UPM Table Configuration Code */
-//	static uint UPMATable[] = {
-//		0xcffffc00, 0x0fffff00, 0x0fafff00, 0x0fafff00,
-//		0x0faffd00, 0x0faffc04, 0x0ffffc00, 0x3ffffc01,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfff7fc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc01,
-//		0xcffffc00, 0x0fffff00, 0x0ff3ff00, 0x0ff3ff00,
-//		0x0ff3fe00, 0x0ffffc00, 0x3ffffc05, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc01,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc00,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc01,
-//		0xfffffc00, 0xfffffc00, 0xfffffc00, 0xfffffc01
-//	};
-//	volatile immap_t *immap = (immap_t *) CONFIG_SYS_IMMR;
-//
-//	set_lbc_br(3, CONFIG_SYS_BR3_PRELIM);
-//	set_lbc_or(3, CONFIG_SYS_OR3_PRELIM);
-//
-//	/* Program the MAMR. RFEN=0, OP=00, UWPL=1, AM=000, DS=01, G0CL=000,
-//	   GPL4=0, RLF=0001, WLF=0001, TLF=0001, MAD=000000
-//	 */
-//	immap->im_lbc.mamr = 0x08404440;
-//
-//	upmconfig(0, UPMATable, sizeof(UPMATable) / sizeof(UPMATable[0]));
-//
-//	puts("UPMA:  Configured for compact flash\n");
-//#endif
-
 	return 0;
 }
 
@@ -263,31 +206,6 @@ int misc_init_r(void)
 	unsigned int orig_bus = i2c_get_bus_num();
 	u8 i2c_data;
 
-//#ifdef CONFIG_SYS_I2C_RTC_ADDR
-//	u8 ds1339_data[17];
-//#endif
-
-//#ifdef CONFIG_SYS_I2C_EEPROM_ADDR
-//	static u8 eeprom_data[] =	/* HRCW data */
-//	{
-//		0xAA, 0x55, 0xAA,       /* Preamble */
-//		0x7C,		        /* ACS=0, BYTE_EN=1111, CONT=1 */
-//		0x02, 0x40,	        /* RCWL ADDR=0x0_0900 */
-//		(CONFIG_SYS_HRCW_LOW >> 24) & 0xFF,
-//		(CONFIG_SYS_HRCW_LOW >> 16) & 0xFF,
-//		(CONFIG_SYS_HRCW_LOW >> 8) & 0xFF,
-//		CONFIG_SYS_HRCW_LOW & 0xFF,
-//		0x7C,		        /* ACS=0, BYTE_EN=1111, CONT=1 */
-//		0x02, 0x41,	        /* RCWH ADDR=0x0_0904 */
-//		(CONFIG_SYS_HRCW_HIGH >> 24) & 0xFF,
-//		(CONFIG_SYS_HRCW_HIGH >> 16) & 0xFF,
-//		(CONFIG_SYS_HRCW_HIGH >> 8) & 0xFF,
-//		CONFIG_SYS_HRCW_HIGH & 0xFF
-//	};
-//
-//	u8 data[sizeof(eeprom_data)];
-//#endif
-
 //	printf("Board revision: ");
 //	i2c_set_bus_num(1);
 //	if (i2c_read(CONFIG_SYS_I2C_8574A_ADDR2, 0, 0, &i2c_data, sizeof(i2c_data)) == 0)
@@ -299,92 +217,8 @@ int misc_init_r(void)
 //		rc = 1;
 //	}
 
-//#ifdef CONFIG_SYS_I2C_EEPROM_ADDR
-//	i2c_set_bus_num(0);
-//
-//	if (i2c_read(CONFIG_SYS_I2C_EEPROM_ADDR, 0, 2, data, sizeof(data)) == 0) {
-//		if (memcmp(data, eeprom_data, sizeof(data)) != 0) {
-//			if (i2c_write
-//			    (CONFIG_SYS_I2C_EEPROM_ADDR, 0, 2, eeprom_data,
-//			     sizeof(eeprom_data)) != 0) {
-//				puts("Failure writing the HRCW to EEPROM via I2C.\n");
-//				rc = 1;
-//			}
-//		}
-//	} else {
-//		puts("Failure reading the HRCW from EEPROM via I2C.\n");
-//		rc = 1;
-//	}
-//#endif
-
-//#ifdef CONFIG_SYS_I2C_RTC_ADDR
-//	i2c_set_bus_num(1);
-//
-//	if (i2c_read(CONFIG_SYS_I2C_RTC_ADDR, 0, 1, ds1339_data, sizeof(ds1339_data))
-//	    == 0) {
-//
-//		/* Work-around for MPC8349E-mITX bug #13601.
-//		   If the RTC does not contain valid register values, the DS1339
-//		   Linux driver will not work.
-//		 */
-//
-//		/* Make sure status register bits 6-2 are zero */
-//		ds1339_data[0x0f] &= ~0x7c;
-//
-//		/* Check for a valid day register value */
-//		ds1339_data[0x03] &= ~0xf8;
-//		if (ds1339_data[0x03] == 0) {
-//			ds1339_data[0x03] = 1;
-//		}
-//
-//		/* Check for a valid date register value */
-//		ds1339_data[0x04] &= ~0xc0;
-//		if ((ds1339_data[0x04] == 0) ||
-//		    ((ds1339_data[0x04] & 0x0f) > 9) ||
-//		    (ds1339_data[0x04] >= 0x32)) {
-//			ds1339_data[0x04] = 1;
-//		}
-//
-//		/* Check for a valid month register value */
-//		ds1339_data[0x05] &= ~0x60;
-//
-//		if ((ds1339_data[0x05] == 0) ||
-//		    ((ds1339_data[0x05] & 0x0f) > 9) ||
-//		    ((ds1339_data[0x05] >= 0x13)
-//		     && (ds1339_data[0x05] <= 0x19))) {
-//			ds1339_data[0x05] = 1;
-//		}
-//
-//		/* Enable Oscillator and rate select */
-//		ds1339_data[0x0e] = 0x1c;
-//
-//		/* Work-around for MPC8349E-mITX bug #13330.
-//		   Ensure that the RTC control register contains the value 0x1c.
-//		   This affects SATA performance.
-//		 */
-//
-//		if (i2c_write
-//		    (CONFIG_SYS_I2C_RTC_ADDR, 0, 1, ds1339_data,
-//		     sizeof(ds1339_data))) {
-//			puts("Failure writing to the RTC via I2C.\n");
-//			rc = 1;
-//		}
-//	} else {
-//		puts("Failure reading from the RTC via I2C.\n");
-//		rc = 1;
-//	}
-//#endif
-
 	i2c_set_bus_num(orig_bus);
 #endif
-
-//#ifdef CONFIG_VSC7385_IMAGE
-//	if (vsc7385_upload_firmware((void *) CONFIG_VSC7385_IMAGE,
-//		CONFIG_VSC7385_IMAGE_SIZE)) {
-//		puts("Failure uploading VSC7385 microcode.\n");
-//		rc = 1;
-//	}
-//#endif
 
 	return rc;
 }
