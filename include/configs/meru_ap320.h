8a14035379004298858b0c65875607679be68542
charles <celliwig@nym.hush.com>
2020-07-28 23:46:20 +0000

    Updated board file/defconfig: 	- Added UBI commands. 	- Increased amount of RAM reserved for malloc.

--- include/configs/meru_ap320.h
+++ include/configs/meru_ap320.h
@@ -118,7 +118,7 @@
 
 /* CONFIG_SYS_MONITOR_LEN must be a multiple of CONFIG_ENV_SECT_SIZE */
 #define CONFIG_SYS_MONITOR_LEN		(768 * 1024)	/* Reserve 512 kB for Mon */
-#define CONFIG_SYS_MALLOC_LEN		(256 * 1024)	/* Reserved for malloc */
+#define CONFIG_SYS_MALLOC_LEN		(32 * 1024 * 1024)	/* Reserved for malloc */
 
 /*
  * Serial Port
